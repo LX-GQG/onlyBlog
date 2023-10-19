437a5488906476e28fc37c610d89a3ecb36baa6e
LX-GQG <1137601326@qq.com>
2023-10-19 16:21:41 +0800

    修复bug

--- config/config.js
+++ config/config.js
@@ -1,17 +1,32 @@
 // 数据库配置文件
+// const config = {
+//     database: {
+//         user: "root", //账号
+//         password: "你自己的数据库密码", //密码
+//         database: "onlyBlog", //数据库
+//         host: "127.0.0.1", //服务器地址或者自己的ip地址
+//         port: 3306, //数据库端口
+//         timezone: '+08:00'
+//     },
+//     // jwt配置参数
+//     tokenConfig: {
+//         serect: 'onlyBlog', //密钥
+//         expiresIn: '24h' //过期时间
+//     }
+// };
 const config = {
     database: {
         user: "root", //账号
-        password: "你自己的数据库密码", //密码
+        password: "wr0925wr", //密码
         database: "onlyBlog", //数据库
-        host: "127.0.0.1", //服务器地址或者自己的ip地址
+        host: "120.25.232.80", //服务器地址或者自己的ip地址
         port: 3306, //数据库端口
         timezone: '+08:00'
     },
     // jwt配置参数
     tokenConfig: {
         serect: 'onlyBlog', //密钥
-        expiresIn: '24h' //过期时间
+        expiresIn: '168h' //过期时间
     }
 };
 
